d6fe4ed62bfe312c061a67d09cd7ff4425f703b7
judykaushalya <judykaushalya3@gmail.com>
2025-04-10 15:33:52 +0530

    US: Project Setup

---
+++ User_service/src/test/java/com/distributedproject/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedproject.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
