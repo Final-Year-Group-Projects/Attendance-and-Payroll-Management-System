8338665fc6bd8fe2f23f06486aa11771bea30317
ThakshiRajapaksha <EN97627@foe.sjp.ac.lk>
2025-04-11 11:57:08 +0530

    Setup springoot project

---
+++ Attendance_service/src/main/java/com/attendance/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.attendance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Attendance Service is running!";
+    }
+}
