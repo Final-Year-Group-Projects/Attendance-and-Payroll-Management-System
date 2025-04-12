0bfb38599bba4e32ceb11dcdd022f6ab5d10c2ef
ThakshiRajapaksha <EN97627@foe.sjp.ac.lk>
2025-04-13 00:31:52 +0530

    added database env variables and fix issue META-INF

--- Attendance_service/src/main/java/com/attendance/controller/TestController.java
+++ Attendance_service/src/main/java/com/attendance/controller/TestController.java
@@ -1,12 +1,30 @@
 package com.attendance.controller;
 
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
+    private final BuildProperties buildProperties;
+
+    public TestController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
     @GetMapping("/test")
-    public String test() {
-        return "Attendance Service is running!";
+    public Map<String, String> test() {
+        Map<String, String> info = new HashMap<>();
+        info.put("artifact", buildProperties.getArtifact());
+        info.put("version", buildProperties.getVersion());
+        info.put("buildTime", buildProperties.getTime().toString());
+        return info;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Attendance Service!";
     }
 }
