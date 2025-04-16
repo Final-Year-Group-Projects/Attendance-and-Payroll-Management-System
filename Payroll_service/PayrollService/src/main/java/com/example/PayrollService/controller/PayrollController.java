53afbda60e8b6dcd5fe65b385f77783561b11365
Oshadi Palliyaguruge <oshadi.palliyaguruge@gmail.com>
2025-04-17 03:10:31 +0530

    Test Application using GET Method - Payroll Service

--- Payroll_service/PayrollService/src/main/java/com/example/PayrollService/controller/PayrollController.java
+++ Payroll_service/PayrollService/src/main/java/com/example/PayrollService/controller/PayrollController.java
@@ -1,4 +1,21 @@
 package com.example.PayrollService.controller;
 
+import com.example.PayrollService.service.PayrollService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class PayrollController {
+    @Autowired
+    private PayrollService payrollService;
+
+
+    @GetMapping("/")
+    public String home() {
+        return "Payroll Service is running!";
+    }
+
+
 }
