5e337b7a4cbe1e7a7b4b4b99830d6289127a1cdc
ThakshiRajapaksha <EN97627@foe.sjp.ac.lk>
2025-05-29 03:14:45 +0530

    Registered all services in eureka discovery

--- Attendance_service/src/main/java/com/attendance/AttendanceServiceApplication.java
+++ Attendance_service/src/main/java/com/attendance/AttendanceServiceApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {
 		SecurityAutoConfiguration.class,
 		ManagementWebSecurityAutoConfiguration.class
 })
+@EnableDiscoveryClient
 @EnableFeignClients
 public class AttendanceServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceServiceApplication.class, args);
 	}
